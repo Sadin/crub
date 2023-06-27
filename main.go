ff4edcb728fcc110174e7442f1593d24d86aad43
Zach Snyder <sadin@fedoraproject.org>
2023-06-27 13:50:39 -0400

    removed localized command handler boilerplate

--- main.go
+++ main.go
@@ -210,24 +210,6 @@ var (
 				},
 			})
 		},
-		"localized-command": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			responses := map[discordgo.Locale]string{
-				discordgo.ChineseCN: "你好！ 这是一个本地化的命令",
-			}
-			response := "Hi! This is a localized message"
-			if r, ok := responses[i.Locale]; ok {
-				response = r
-			}
-			err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Content: response,
-				},
-			})
-			if err != nil {
-				panic(err)
-			}
-		},
 		"options": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			// Access options in the order provided by the user.
 			options := i.ApplicationCommandData().Options
